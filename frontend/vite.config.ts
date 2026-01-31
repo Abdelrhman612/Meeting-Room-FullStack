7ff2bac203926d9d40ab9ff7e4e0ec97aac3b9f6
Abdelrhman <abdelrahman.ayman.tech@gmail.com>
2026-02-01 00:21:58 +0200

    update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import dotenv from 'dotenv'
+import tailwindcss from '@tailwindcss/vite'
 dotenv.config()
 // https://vite.dev/config/
 export default defineConfig({
+   plugins: [
+    tailwindcss(),
+  ],
 server: {
   port: Number(process.env.PORT) || 3000,
 }
