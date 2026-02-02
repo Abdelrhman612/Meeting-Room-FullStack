50e726e5ce346f37e67dd460fb92e9d95135c42f
Abdelrhman <abdelrahman.ayman.tech@gmail.com>
2026-02-02 22:00:01 +0200

    update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import "./App.css";
-import MeetingRoom from "./pages/MeetingRoom";
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/meeting-room" element={<MeetingRoom />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the App</h1>
+      </header>
+    </div>
   );
 }
 
