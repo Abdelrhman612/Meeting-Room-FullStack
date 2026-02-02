471737fd7ecb08441206570e80278025635ecfc2
Abdelrhman <abdelrahman.ayman.tech@gmail.com>
2026-02-02 21:57:58 +0200

    update MeetingHub.cs

--- backend/Hubs/MeetingHub.cs
+++ backend/Hubs/MeetingHub.cs
@@ -1,15 +1,32 @@
+using backend.Models;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
 
 namespace JobMeeting.Api.Hubs
 {
+
+
     public class MeetingHub : Hub
     {
-        public async Task JoinRoom(string roomId)
+        private readonly AppDbContext _context;
+        public MeetingHub(AppDbContext context)
         {
-            await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
-            await Clients.OthersInGroup(roomId)
+            _context = context;
+        }
+        public async Task JoinRoom(string roomCode)
+        {
+            var room = await _context.Rooms
+                .FirstOrDefaultAsync(r => r.Code == roomCode);
+
+            if (room == null)
+                throw new HubException("Room not found");
+
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomCode);
+
+            await Clients.OthersInGroup(roomCode)
                 .SendAsync("UserJoined", Context.ConnectionId);
         }
+
 
         public async Task SendOffer(string roomId, string offer)
         {
