0a9ab63650b0430022bfe82b9476b5980d68768c
Abdelrhman <abdelrahman.ayman.tech@gmail.com>
2026-02-02 20:30:37 +0200

    feat: create Room.cs, 20260202182944_CreateNewTableIsARoom.Designer.cs and 20260202182944_CreateNewTableIsARoom.cs

---
+++ backend/DataBase/Models/Room.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using backend.Data.Models;
+
+namespace backend.DataBase.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Code { get; set; } = null!; 
+        
+        [Required]
+        public string Name { get; set; } = null!;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public int HostUserId { get; set; }
+        public User HostUser { get; set; } = null!;
+    }
+
+}
+
+
+
+
+
