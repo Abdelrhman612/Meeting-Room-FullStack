d48fc258ef8a2ff697d1665eeedc8a78d15f1402
Abdelrhman <abdelrahman.ayman.tech@gmail.com>
2026-02-04 00:25:15 +0200

    feat: create SginInDto.cs and SginUpDto.cs

---
+++ backend/Dto/Auth/SginUpDto.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using backend.Data.Models;
+
+namespace backend.Dto.Auth
+{
+    public class SginUpDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public UserRole Role { get; set; } = UserRole.User;
+
+    }
+}
