3a5546625b07b3d18dd2faf0bb6aece7bc017d8f
Abdelrhman <abdelrahman.ayman.tech@gmail.com>
2026-02-04 00:25:32 +0200

    feat: create 20260203214106_ChangeCloumnInTableUserIsRole.Designer.cs and 20260203214106_ChangeCloumnInTableUserIsRole.cs

---
+++ backend/Migrations/20260203214106_ChangeCloumnInTableUserIsRole.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeCloumnInTableUserIsRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Role",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true,
+                oldDefaultValue: "user");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Role",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                defaultValue: "user",
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
