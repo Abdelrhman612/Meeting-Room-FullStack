f3f173b01771e11b8fe9cfbb6b7b1a4f4acc6296
Abdelrhman <abdelrahman.ayman.tech@gmail.com>
2026-02-04 00:25:23 +0200

    feat: create AuthController.cs

---
+++ backend/Controllers/Auth/AuthController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using backend.Dto.Auth;
+using backend.InterFaces.Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers.Auth
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _auth;
+        
+        public AuthController(IAuthService auth)
+        {
+            _auth = auth;
+        }
+        [HttpPost("SginUp")]
+        public async Task<SginUpDto> SginUp(SginUpDto sginUpDto)
+        {
+            var user = await _auth.SginUp(sginUpDto);
+            return user;
+        }
+        [HttpPost("SginIn")]
+        public async Task<string> SginIn(SginInDto sginInDto)
+        {
+            var token = await _auth.SginIn(sginInDto);
+            return token;
+        }
+
+    }
+}
