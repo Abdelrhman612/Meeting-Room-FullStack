250dbba28c771e8931200b1c58355630d6d6c3f1
Abdelrhman <abdelrahman.ayman.tech@gmail.com>
2026-02-05 23:28:40 +0200

    update 11 files

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fabcafd05c401dda39b59d9a0f0b33832ef21d49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e2c7bd72825c19b870cb00f5aec291fc5aa1edd")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
